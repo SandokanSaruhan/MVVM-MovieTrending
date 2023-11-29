32cc504684beb5724d43bb4f7b068cccf520d9fd
Sandokan Saruhan KOLE <sandokankole@gmail.com>
2023-11-29 20:07:03 +0300

    Initial Commit

---
+++ MVVM MovieTrending/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM MovieTrending
+//
+//  Created by Kole, Saruhan (401-Extern) on 29.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
